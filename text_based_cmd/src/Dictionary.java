5ad8a180b8bc94eb4df77ba185c4409211eb50d4
BrockMorris42611 <48562356+BrockMorris42611@users.noreply.github.com>
2021-03-15 23:57:50 -0400

    Update Dictionary.java

--- text_based_cmd/src/Dictionary.java
+++ text_based_cmd/src/Dictionary.java
@@ -1,15 +1,12 @@
 import java.security.KeyException;
 
 
-/*Create a class that implements a dictionary, associating text strings
-with doubles.  It should use a hash table with the hash function
-described in class. (Break the string into groups of two 8 bit characters.
-Shift one of them 8 bits to the left and OR in the other.  Add all of these
-together, throwing away the carry from the 16th bit.  Divide by a convenient
-number which should be the table size and take the remainder.)
-The class should have at least a constructor and methods to insert and retrieve.
-More methods can be added as you need them later.*/
-
+/*
+-Create a class that implements a dictionary, associating text strings with doubles.
+-hash table with the hash function
+-I Break the string into groups of two 8 bit characters. Then, shift one of them 8 bits to the left and OR in the other. Add all of these
+together, throwing away the carry from the 16th bit. Divide by a convenient number which should be the table size and take the remainder
+*/
 public class Dictionary
 {
   private LL<Variable> theTable[];
@@ -45,22 +42,7 @@ public class Dictionary
       theTable[where].insertAtHead(new Variable(s, val));
       //lookup first and change if it already exists, otherwise insert as above
 
-
-      //int where = hash(s); theTable[where].insertAtHead(new Variable(s, val));
   }
-
-  /*public double find(String s) throws KeyException {
-    int where = hash(s);
-    Variable value = theTable[where].find(s);
-    if(value != null)
-    {
-      return value.getValue();
-    }
-    else
-    {
-      throw new KeyException("Key not found:" + s);
-    }
-  }*/
 
   private int hash(String s)
   {
